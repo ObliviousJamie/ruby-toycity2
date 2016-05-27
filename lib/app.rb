c8138d8024c61f9a87069fc9af28dba24d2dfb15
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-05-27 21:08:53 +0100

    Prints brands

--- lib/app.rb
+++ lib/app.rb
@@ -9,9 +9,14 @@ def setup_files
     $report_file = File.new("report.txt", "w+")
 end
 
+#Write to file
+def print_this(*phrases)
+    phrases.each{|phrase| $report_file.write(" #{phrase} \n")}
+end
+
 #Line break of stars
 def star_line_break
-    puts "**************************"
+    print_this "**************************"
 end
 
 #Prints a given word in ascii art
@@ -21,10 +26,12 @@ end
 #Prints sales and brands secton
 def print_data
     print_heading("Products")
-    puts "\n"
+    print_this "\n"
     makes_products_section
-    puts "\n"
+    print_this "\n"
     print_heading("Brands")
+    print_this "\n"
+    makes_brands_section
 end
 
 # Print "Sales Report" in ascii art
@@ -42,7 +49,7 @@ end
 	# Calculate and print the average discount (% or $) based off the average sales price
 def makes_products_section
     $products_hash["items"].each do |product|
-        puts product["title"]
+        print_this product["title"]
         star_line_break
         
         retail_price = product["full-price"]
@@ -51,12 +58,7 @@ def makes_products_section
         average_price = calculate_average_price total_sales, total_purchases
         average_discount = calculate_average_discount retail_price.to_f, average_price.to_f
         
-        puts "Retail Price: $#{retail_price} "
-        puts "Total Purchases: #{total_purchases} "
-        puts "Total Sales: $#{total_sales.to_f.round(2)} "
-        puts "Average Price: $#{average_price.to_f.round(2)}"
-        puts "Average Discount Percentage: #{average_discount.to_f.round(2)}"
-        puts "\n"
+        print_this "Retail Price: $#{retail_price} ", "Total Purchases: #{total_purchases} ", "Total Sales: $#{total_sales.to_f.round(2)} ","Average Price: $#{average_price.to_f.round(2)}", "Average Discount Percentage: #{average_discount.to_f.round(2)}%", "\n"
     end
 end
 
@@ -83,12 +85,53 @@ end
 	# Calculate and print the average price of the brand's toys
 	# Calculate and print the total sales volume of all the brand's toys combined
 def makes_brands_section
+    items = $products_hash["items"]
+    brands = items.map {|item| item["brand"]}.uniq
 
+    brands.each do |brand|
+        by_brand = items.select { |item| item["brand"] == brand}
+
+        stock = 0
+        total_brand_price = 0
+        total_revenue = 0
+        brand_count = by_brand.length
+     
+        by_brand.each do |key|
+            stock = calculate_brand_stock key,stock
+            total_brand_price = calculate_total_brand_price key,total_brand_price 
+            total_revenue = calculate_total_brand_sales key, total_revenue
+        end 
+        print_this "Brand #{brand} "
+        star_line_break
+
+        avg_price = calculate_avg_brand_price total_brand_price, brand_count
+
+        print_this "Number of Products: #{stock} ", "Average Product Price $#{avg_price}", "Total Sales: $#{total_revenue} ", "\n"
+
+    end
+
+end
+
+def calculate_brand_stock(key,stock)
+    stock + key["stock"].to_i
+end
+
+def calculate_total_brand_price(key,total)
+    (key["full-price"].to_f + total.to_f)
+
+end
+
+def calculate_avg_brand_price(total,items)
+    (total / items).round(2)
+end
+
+def calculate_total_brand_sales(key,total)
+    (total + key["purchases"].inject(0) { |purchase_total, purchase| purchase_total + purchase["price"]}).round(2)
 end
 
 def create_report
     print_heading("Sales report")
-    puts (Date.today)
+    print_this (Date.today)
     print_data
 end
 
